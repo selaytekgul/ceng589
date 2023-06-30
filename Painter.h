b9121ab36ce216c2e9efb02704326ae10f5317d6
selorey <selaytekgul@gmail.com>
2023-06-30 15:55:23 +0300

    Everything is moved to Painter from the main.

--- Painter.h
+++ Painter.h
@@ -13,4 +13,9 @@ class Painter
 {
 public:
 	SoSeparator* getShapeSep(Mesh* mesh);
+	void triangleNormalVectors(Mesh* mesh);
+	float calculateLength(int v[]);
+	void normalize(int v[], float nor[]);
+	void Painter::crossProductFunction(int v_A[], int v_B[], int c_P[]);
+	float Painter::rayIntersectsTriangle(float* p, float* d, float* v0, float* v1, float* v2);
 };
