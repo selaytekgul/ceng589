9994a2c069400bf0d848d26f901eaddd2a5961be
selorey <selaytekgul@gmail.com>
2023-07-02 15:34:38 +0300

    #include <array> is deleted from Mesh.h and TriangleMeshMath.h

--- Mesh.h
+++ Mesh.h
@@ -2,7 +2,6 @@
 
 #include <iostream>
 #include <vector>
-#include <array>
 #include "TypeDefinitions.h"
 
 struct Vertex
--- TriangleMeshMath.h
+++ TriangleMeshMath.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <array>
 #include "Mesh.h"
 #include "TypeDefinitions.h"
 
