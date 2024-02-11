ad16e77964f499ee2d39ed4d77f3679b6b4d44c6
selorey <selaytekgul@gmail.com>
2024-02-11 14:02:06 +0300

    Unnecessary comments are deleted on main, new colorIndex setting line is added on Segmentor.cpp.

--- Main.cpp
+++ Main.cpp
@@ -13,38 +13,20 @@ int main(int, char ** argv)
 	SoSeparator* root = new SoSeparator;
 	root->ref();
 
-	//stuff to be drawn on screen must be added to the root
-//	SoCone * cone = new SoCone;
-//	root->addChild(cone);
-
 	Mesh* mesh = new Mesh();
 	Painter* painter = new Painter();
 
 	//mesh->loadOff("tr_reg_000.off");
-	mesh->loadOff("0.off");
+	//mesh->loadOff("0.off");
 	//mesh->loadOff("car.off");
-	//mesh->loadOff("coffeecup.off");
+	mesh->loadOff("coffeecup.off");
 	//mesh->loadOff("bunny.off");
 	//mesh->loadOff("cube3.off");
 //	mesh->createCube(20.0f);
 
 	Segmentor segmentor(mesh);
-	segmentor.assignDiameterValuesOfVertices();
+	//segmentor.assignDiameterValuesOfVertices();
 	segmentor.setColorValuesToVertices();
-
-	//cout << "my (verts[4]) 1-ring neighborhood is: \n";
-	//for (int nv = 0; nv < mesh->verts[4]->vertList.size(); nv++)
-	//	cout << mesh->verts[4]->vertList[nv] << " neighbb\n";
-
-
-	//cout << "my (verts[4]) 1-ring neighborhood is: \n";
-	//for (int ne = 0; ne < mesh->verts[4]->edgeList.size(); ne++)
-	//	if (mesh->edges[   mesh->verts[4]->edgeList[ne]   ]->v1i == 4)
-	//		cout << mesh->edges[   mesh->verts[4]->edgeList[ne]   ]->v2i << " nnnnnnnnn\n";
-	//	else
-	//		cout << mesh->edges[   mesh->verts[4]->edgeList[ne]   ]->v1i << " nnnnnnnnn\n";
-
-//		cout << mesh->verts[4]->vertList[nv] << " neighbb\n";
 
 	root->addChild( painter->getShapeSep(mesh) );
 
--- Segmentor.cpp
+++ Segmentor.cpp
@@ -105,7 +105,12 @@ void Segmentor::setColorValuesToVertices()
 	//assign color values to the vertices according to their diameter values
 	for (Vertex* vertex : mesh->verts)
 	{
+		// setting the color for a vertex
 		int colorIndex = static_cast<int>(vertex->diameter / k);
+		
+		colorIndex = static_cast<int>(vertex->diameter / k);
+		
+		//index out of bounds error check for colors array
 		if (colorIndex > usedNumberOfColors - 1)
 			colorIndex = usedNumberOfColors - 1;
 
