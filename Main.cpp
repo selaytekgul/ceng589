076ae512d8ff16f26ba82812be2424674cc6d730
selorey <selaytekgul@gmail.com>
2024-03-31 12:58:03 +0300

    comments are deleted

--- Main.cpp
+++ Main.cpp
@@ -85,57 +85,5 @@ int main(int, char ** argv)
 	mesh->windingNumberByYusufSahillioglu(&v3);
 
 	int b = 9;
-	//cout << "my (verts[4]) 1-ring neighborhood is: \n";
-//for (int nv = 0; nv < mesh->verts[4]->vertList.size(); nv++)
-//	cout << mesh->verts[4]->vertList[nv] << " neighbb\n";
-
-
-//cout << "my (verts[4]) 1-ring neighborhood is: \n";
-//for (int ne = 0; ne < mesh->verts[4]->edgeList.size(); ne++)
-//	if (mesh->edges[   mesh->verts[4]->edgeList[ne]   ]->v1i == 4)
-//		cout << mesh->edges[   mesh->verts[4]->edgeList[ne]   ]->v2i << " nnnnnnnnn\n";
-//	else
-//		cout << mesh->edges[   mesh->verts[4]->edgeList[ne]   ]->v1i << " nnnnnnnnn\n";
-
-//		cout << mesh->verts[4]->vertList[nv] << " neighbb\n";
-
-	//root->addChild( painter->getShapeSep(mesh) );
-	
-	//Part-1
-	//std::srand(std::time(NULL)); //seeding for the first time only!
-	//const int min = 0;
-	//const int max = mesh->verts.size();
-	//const int range = max - min + 1;
-	//const int num1 = std::rand() % range + min;
-	//const int num2 = std::rand() % range + min;
-	//const int source_vert_Id = 213;
-	//const int dest_vert_Id = 451;
-	//mesh->samples = { mesh->verts[source_vert_Id]->idx , mesh->verts[dest_vert_Id]->idx };
-	//Graph gmesh = Dijkstra::meshToGraph(mesh);
-	//Dijkstra::timing(gmesh, source_vert_Id, dest_vert_Id, Dijkstra::FIB_HEAP);
-	//Dijkstra::fprinting(gmesh, Dijkstra::FIB_HEAP);
-	//Dijkstra::fprintingOnce(gmesh,source_vert_Id, dest_vert_Id, Dijkstra::ARRAY);
-	//Dijkstra::pathDrawing(mesh, gmesh, source_vert_Id, dest_vert_Id, Dijkstra::MIN_HEAP);
-
-	//for (size_t i = 0; i < mesh->edges.size(); i++)
-	//{
-	//	//if (mesh->edges[i]->isItBoundary)
-	//	//if (mesh->edges[i]->isItInLongestBoundary)
-	//	//if (mesh->edges[i]->isItPathPart)
-	//	//if (mesh->edges[i]->isItTraversed)
-	//	if (mesh->edges[i]->isInShortestPath)
-	//		root->addChild(painter->getThickLineSep(mesh, i));
-	//}
-
-	//root->addChild(painter->getSpheresSep(mesh, 0.f, 0.f, 1.f));
-
-	//viewer->setSize(SbVec2s(640, 480));
-	//viewer->setSceneGraph(root);
-	//viewer->show();
-
-	//SoWin::show(window);
-	//SoWin::mainLoop();
-	//delete viewer;
-	//root->unref();
 	return 0;
 }
