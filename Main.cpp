19f5704f23157d7100aec744a0223b5ecdc03312
selorey <selaytekgul@gmail.com>
2024-03-31 13:05:00 +0300

    winding number test is functionated

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,23 @@
 
 #include <Eigen/Dense>
 
+void windingNumberTest(Mesh* mesh)
+{
+	mesh->windingNumberByYusufSahillioglu(mesh->verts[0]);
+	mesh->windingNumberByYusufSahillioglu(mesh->verts[1]);
+	for (size_t i = 0; i < mesh->verts.size(); i++)
+	{
+		mesh->windingNumberByYusufSahillioglu(mesh->verts[i]);
+		if (mesh->verts[i]->winding == 1)
+		{
+			std::cout << i << ": " << mesh->verts[i]->winding << std::endl;
+		}
+	}
+	float ptr3[3] = { -5.8959f, -16.9698f, 128.8068f };
+	Vertex v3 = { (int)mesh->verts.size(), ptr3 };
+	mesh->windingNumberByYusufSahillioglu(&v3);
+}
+
 int main(int, char ** argv)
 {
 	Mesh* mesh = new Mesh();
@@ -41,29 +58,6 @@ int main(int, char ** argv)
 
 	mesh->loadOff(fileName + ".off");
 
-	mesh->windingNumberByYusufSahillioglu(mesh->verts[0]);
-	mesh->windingNumberByYusufSahillioglu(mesh->verts[1]);
-	for (size_t i = 0; i < mesh->verts.size(); i++)
-	{
-		mesh->windingNumberByYusufSahillioglu(mesh->verts[i]);
-		if (mesh->verts[i]->winding == 1)
-		{
-			int c = 7;
-			std::cout << i << ": " << mesh->verts[i]->winding << std::endl;
-		}
-	}
-	int a = 5;
-	//float ptr[3] = {-15.f, 10.f, -5.f };
-	//float ptr1[3] = {-45.f, 10.f, -5.f };
-	//float ptr1[3] = {45.f, 10.f, -5.f };
-	float ptr3[3] = {-5.8959f, -16.9698f, 128.8068f };
-	//Vertex v = { (int)mesh->verts.size(), ptr };
-	//mesh->windingNumberByYusufSahillioglu(&v);
-	//Vertex v1 = { (int)mesh->verts.size(), ptr1 };
-	//mesh->windingNumberByYusufSahillioglu(&v1);
-	Vertex v3 = { (int)mesh->verts.size(), ptr3 };
-	mesh->windingNumberByYusufSahillioglu(&v3);
-
-	int b = 9;
 	return 0;
 }
+
