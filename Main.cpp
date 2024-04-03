3406f15ba50a1d2d4aadb5c1f7c84df1a582ef59
selorey <selaytekgul@gmail.com>
2024-04-03 22:32:41 +0300

    Dijkstra imp removed deleted

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,5 @@
 #define HAVE_INT8_T
 #include "GraphOperations.h"
-#include "DijstraImplementations.h"
 #include <random>
 #include <string>
 #include <queue>
@@ -28,36 +27,7 @@ int main(int, char ** argv)
 {
 	Mesh* mesh = new Mesh();
 	Mesh* original_mesh = new Mesh();
-
-	//std::string fileName = { "tr_reg_000" };
-	//
-	//std::string fileName = { "for timing/centaur" };
-	//std::string fileName = { "for timing/man" };
-	//
-	//std::string fileName = { "for timing/weirdSphere" };
-	//std::string fileName = { "for fprinting/horse0" };
-	//std::string fileName = { "for fprinting/man0" };
-
-	//std::string fileName = { "0" };
-	//std::string fileName = { "1" };
-	//std::string fileName = { "car" };
-	//std::string fileName = { "coffeecup" };
-	//std::string fileName = { "bunny" };
-	//std::string fileName = { "cube3" };
 	std::string fileName = { "equal_cube3" };
-	
-	//std::string fileName = { "faces/face" };
-	//std::string fileName = { "faces/face-low" };
-	//std::string fileName = { "faces/facem" };
-	//std::string fileName = { "faces/facem-low" };
-	
-	//std::string fileName = { "doubleOpenCube3" };
-	//std::string fileName = { "man0" };
-
-	//mesh->createCube(20.0f);
-	//mesh->createOpenCube(20.0f);
-	//mesh->createDoubleOpenCube(20.0f);
-
 	mesh->loadOff(fileName + ".off");
 	original_mesh->loadOff(fileName + ".off");
 
